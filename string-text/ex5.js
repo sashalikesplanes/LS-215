db2faa9568a56c777c73f6a2a6a8c7cdeb8d5d5f
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-07-31 19:15:54 -0400

    solved ex

---
+++ string-text/ex5.js
@@ -0,0 +1,11 @@
+function staggeredCase(str) {
+  return str.split('')
+            .map((char, idx) => {
+              if (idx % 2 === 0) return char.toUpperCase();
+              else return char.toLowerCase();
+            })
+            .join('');
+}
+console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
+console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
+console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
