fc3ad9a86a21cfcd34f98703e4ea5d93b359c76e
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-07-31 22:33:51 -0400

    started ex

---
+++ lesson3/ex1.js
@@ -0,0 +1,18 @@
+// PEDAC
+// Print a diamond shape consisting of *s
+// Algorithm
+//
+//
+// Input
+// integer
+// output
+// string with \n delimiters
+// Model
+// For an odd integer
+// make a list of [1, 3, ..., 3, 1] the number of stars per row
+// transform into a string 
+// add white space
+//
+function diamond(n) {
+  const 
+}
